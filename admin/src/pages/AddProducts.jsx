7639ecb662458360d2a6d53066363148e0946d2f
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-01-14 10:44:41 +0530

    added shadcn components

--- admin/src/pages/AddProducts.jsx
+++ admin/src/pages/AddProducts.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { assets } from "../admin_assets/assets";
+import { SaveButton } from "@/components/ui/status-button";
+import { toast } from "sonner";
 
 const AddProducts = () => {
   return (
@@ -23,7 +25,6 @@ const AddProducts = () => {
           />
         </label>
       </div>
-
       <div className="w-1/4">
         <p className="mb-2">Product Name</p>
         <input
@@ -34,15 +35,65 @@ const AddProducts = () => {
         />
       </div>
 
-      <div>
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
         <div>
-          <p>Product Category</p>
-          <select>
+          <p className="mb-2">Product Category</p>
+          <select className="w-full px-3 py-2">
             <option value="Men">Men</option>
             <option value="Women">Women</option>
           </select>
         </div>
+        <div>
+          <p className="mb-2">Sub-Category</p>
+          <select className="w-full px-3 py-2">
+            <option value="Casual">Casual</option>
+            <option value="Sports">Sports</option>
+            <option value="Formal">Formal</option>
+          </select>
+        </div>
+
+        <div>
+          <p className="mb-2">Product Price</p>
+          <input
+            className="w-full px-3 py-2 sm:w-[120px]"
+            type="Number"
+            placeholder="999"
+            required
+          />
+        </div>
       </div>
+
+      <div>
+        <p className="mb-2">Product Sizes</p>
+        <div className="flex gap-3">
+          <div>
+            <p className="bg-slate-200 px-3 py-1 cursor-pointer">UK-5</p>
+          </div>
+          <div>
+            <p className="bg-slate-200 px-3 py-1 cursor-pointer">UK-6</p>
+          </div>
+          <div>
+            <p className="bg-slate-200 px-3 py-1 cursor-pointer">UK-7</p>
+          </div>
+          <div>
+            <p className="bg-slate-200 px-3 py-1 cursor-pointer">UK-8</p>
+          </div>
+          <div>
+            <p className="bg-slate-200 px-3 py-1 cursor-pointer">UK-9</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex gap-2 mt-2">
+        <input type="checkbox" id="bestSeller" />
+        <label className="cursor-pointer" htmlFor="bestSeller">
+          Add to Best Seller
+        </label>
+      </div>
+
+      <SaveButton
+        onComplete={() => toast.success("Product added successfully")}
+      />
     </form>
   );
 };
