048fc59f40ca0039996239932f47376966a0a96d
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-01-23 17:51:21 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ const flash = require("connect-flash");
 const connectCloudinary = require("./config/cloudinary");
 connectCloudinary();
 
-// CORS Configuration - UPDATED
 const allowedOrigins = [
     // Development
     "http://localhost:5173",
@@ -30,29 +29,38 @@ const allowedOrigins = [
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
 
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            console.log('Blocked origin:', origin);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Set-Cookie"]
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+app.options("*", cors());
+
 app.use(cookieParser());
+app.set("trust proxy", 1);
+
 app.use(
-    expressSession({
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.EXPRESS_SESSION_SECRET
-    })
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    cookie: {
+      secure: true,
+      sameSite: "none"    
+    }
+  })
 );
+
 app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
